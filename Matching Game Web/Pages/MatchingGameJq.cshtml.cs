6c994e0848167c436540260d1b81d209210d42ce
shiffygreen <shiffygreen73@gmail.com>
2024-12-25 21:38:35 -0500

    added jquery

---
+++ Matching Game Web/Pages/MatchingGameJq.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Matching_Game_Web.Pages
+{
+    public class MatchingGameJqModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
